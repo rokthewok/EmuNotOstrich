7372a7249d82b8419090b5ebde15f0001d8cdd32
Kevin Pike <kevin.e.pike@gmail.com>
2013-01-11 18:06:06 -0500

    Added register class with tests.

---
+++ src/GameBoyRegisters.java
@@ -0,0 +1,150 @@
+
+public class GameBoyRegisters {
+	//Array to hold registers
+	private byte registers[];
+	//Enum used to refer to registers
+	public enum Register {
+		A, B, C, D, E, F, H, L, AF, BC, DE, HL, SP, PC
+	}
+	
+	public GameBoyRegisters() {
+		registers = new byte[12]; 
+	}
+	
+	public void setRegister(Register reg, short data) {
+		switch (reg) {
+		case AF:
+			this.set16BitRegisterFromTwo8Bit(Register.A, Register.F, data);
+			break;
+		case BC:
+			this.set16BitRegisterFromTwo8Bit(Register.A, Register.F, data);
+			break;
+		case DE:
+			this.set16BitRegisterFromTwo8Bit(Register.A, Register.F, data);
+			break;
+		case HL:
+			this.set16BitRegisterFromTwo8Bit(Register.A, Register.F, data);
+			break;
+		case PC:
+			this.set16BitRegister(reg, data);
+			break;
+		case SP:
+			this.set16BitRegister(reg, data);
+		default:
+			this.set8BitRegister(reg, data);
+			break;
+		}
+	}
+	
+	private void set8BitRegister(Register reg, short data) {
+		registers[this.getRegisterIndex(reg)] = (byte)data;
+	}
+	
+	private void set16BitRegisterFromTwo8Bit(Register x, Register y, short data) {
+		byte xData, yData;
+		xData = (byte) ((data & 240) >> 16); // 1111 0000 = 240
+		yData = (byte) (data & 15); // 0000 1111 = 15
+		registers[this.getRegisterIndex(x)] = xData;
+		registers[this.getRegisterIndex(y)] = yData;
+	}
+	
+	private void set16BitRegister(Register reg, short data) {
+		byte first, second;
+		first = (byte) ((data & 240) >> 16); // 1111 0000 = 240
+		second = (byte) (data & 15); // 0000 1111 = 15
+		int index = this.getRegisterIndex(reg);
+		registers[index] = first;
+		registers[index + 1] = second;
+	}
+	
+	/**
+	 * Returns the data in the specified register
+	 * @param reg desired register
+	 * @return data in register
+	 */
+	public short getRegister(Register reg) {
+		short register = 0;
+
+		switch (reg) {
+		case AF:
+			register = this.combine8BitRegisters(Register.A, Register.B);
+			break;
+		case BC:
+			register = this.combine8BitRegisters(Register.A, Register.B);
+			break;
+		case DE:
+			register = this.combine8BitRegisters(Register.D, Register.E);
+
+			break;
+		case HL:
+			register = this.combine8BitRegisters(Register.H, Register.L);
+			break;
+		default:
+			register = registers[this.getRegisterIndex(reg)];
+			break;
+		}
+		
+		return register;
+	}
+	
+	/**
+	 * Gets the index of the register in registers
+	 * @param reg Game boy register
+	 * @return index of register in registers
+	 */
+	private int getRegisterIndex(Register reg) {
+		int index = 0;
+		
+		switch (reg) {
+		case A:
+			index = 0;
+			break;
+		case B:
+			index = 1;
+			break;
+		case C:
+			index = 2;
+			break;
+		case D:
+			index = 3;
+			break;
+		case E:
+			index = 4;
+			break;
+		case F:
+			index = 5;
+			break;
+		case H:
+			index = 6;
+			break;
+		case L:
+			index = 7;
+			break;
+		case PC:
+			index = 8;
+			break;
+		case SP:
+			index = 10;
+			break;
+		default:
+			// All other cases are not necessary
+			break;
+		
+		}
+		return index;
+	}
+	
+	/**
+	 * Combines register 1 and 2 by shifting register 1 to the left 
+	 * and or'ing register 2
+	 * @param x register 1
+	 * @param y register 2
+	 * @return register 1 and register 2 combined
+	 */
+	private short combine8BitRegisters(Register x, Register y) {
+		
+		short X = this.registers[this.getRegisterIndex(x)];
+		short Y = this.registers[this.getRegisterIndex(y)];
+		return (short) (X << 8 | Y);
+	}
+}
---
+++ src/GameBoyRegistersTests.java
@@ -0,0 +1,27 @@
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class GameBoyRegistersTests {
+
+	private GameBoyRegisters registers;
+	@Before
+	public void setUp() throws Exception {
+		registers = new GameBoyRegisters();
+	}
+
+	@Test
+	public void testRegisters() {
+		registers.setRegister(GameBoyRegisters.Register.A, (short)12);
+		Assert.assertEquals(registers.getRegister(GameBoyRegisters.Register.A), 12);
+		registers.setRegister(GameBoyRegisters.Register.DE, (short)12);
+		Assert.assertEquals(registers.getRegister(GameBoyRegisters.Register.D), 0);
+		Assert.assertEquals(registers.getRegister(GameBoyRegisters.Register.E), 12);
+		registers.setRegister(GameBoyRegisters.Register.PC, (short)12);
+		Assert.assertEquals(registers.getRegister(GameBoyRegisters.Register.PC), 12);
+	}
+
+}
