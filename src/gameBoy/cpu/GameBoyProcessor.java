f36b1a39e95aaff8d9195c6d77ec7ea20476057b
rokthewok <jqruffer@gmail.com>
2013-01-12 13:36:04 -0500

    Added switch statements to replace using individual classes for each opcode

--- src/gameBoy/cpu/GameBoyProcessor.java
+++ src/gameBoy/cpu/GameBoyProcessor.java
@@ -59,8 +59,699 @@ public class GameBoyProcessor implements IProcessor {
 	private void switchHighNibble( byte opcode ) {
 		byte highNibble = (byte) ( opcode >> 4 );
 		byte lowNibble = (byte) ( opcode & 0x0F );
-		switch( highNibble ) {
 		
+		switch( highNibble ) {
+		case 0x0:
+			this.switch0LowNibble( opcode, lowNibble );
+			break;
+		case 0x1:
+			this.switch1LowNibble( opcode, lowNibble );
+			break;
+		case 0x2:
+			this.switch2LowNibble( opcode, lowNibble );
+			break;
+		case 0x3:
+			this.switch3LowNibble( opcode, lowNibble );
+			break;
+		case 0x4:
+			this.switch4LowNibble( opcode, lowNibble );
+			break;
+		case 0x5:
+			this.switch5LowNibble( opcode, lowNibble );
+			break;
+		case 0x6:
+			this.switch6LowNibble( opcode, lowNibble );
+			break;
+		case 0x7:
+			this.switch7LowNibble( opcode, lowNibble );
+			break;
+		case 0x8:
+			this.switch8LowNibble( opcode, lowNibble );
+			break;
+		case 0x9:
+			this.switch9LowNibble( opcode, lowNibble );
+			break;
+		case 0xA:
+			this.switchALowNibble( opcode, lowNibble );
+			break;
+		case 0xB:
+			this.switchBLowNibble( opcode, lowNibble );
+			break;
+		case 0xC:
+			this.switchCLowNibble( opcode, lowNibble );
+			break;
+		case 0xD:
+			this.switchDLowNibble( opcode, lowNibble );
+			break;
+		case 0xE:
+			this.switchELowNibble( opcode, lowNibble );
+			break;
+		case 0xF:
+			this.switchFLowNibble( opcode, lowNibble );
+			break;
+		default:
+			System.out.printf( "Opcode 0x%2x not valid or implemented opcode", opcode );
+			break;
+		}
+	}
+	
+	private void switch0LowNibble( byte opcode, byte lowNibble ) {
+		switch( lowNibble ) {
+		case 0x0:
+			break;
+		case 0x1:
+			break;
+		case 0x2:
+			break;
+		case 0x3:
+			break;
+		case 0x4:
+			break;
+		case 0x5:
+			break;
+		case 0x6:
+			break;
+		case 0x7:
+			break;
+		case 0x8:
+			break;
+		case 0x9:
+			break;
+		case 0xA:
+			break;
+		case 0xB:
+			break;
+		case 0xC:
+			break;
+		case 0xD:
+			break;
+		case 0xE:
+			break;
+		case 0xF:
+			break;
+		default:
+			System.out.printf( "Opcode 0x%2x not valid or implemented opcode", opcode );
+			break;
+		}
+	}
+	
+	private void switch1LowNibble( byte opcode, byte lowNibble ) {
+		switch( lowNibble ) {
+		case 0x0:
+			break;
+		case 0x1:
+			break;
+		case 0x2:
+			break;
+		case 0x3:
+			break;
+		case 0x4:
+			break;
+		case 0x5:
+			break;
+		case 0x6:
+			break;
+		case 0x7:
+			break;
+		case 0x8:
+			break;
+		case 0x9:
+			break;
+		case 0xA:
+			break;
+		case 0xB:
+			break;
+		case 0xC:
+			break;
+		case 0xD:
+			break;
+		case 0xE:
+			break;
+		case 0xF:
+			break;
+		default:
+			System.out.printf( "Opcode 0x%2x not valid or implemented opcode", opcode );
+			break;
+		}
+	}
+	
+	private void switch2LowNibble( byte opcode, byte lowNibble ) {
+		switch( lowNibble ) {
+		case 0x0:
+			break;
+		case 0x1:
+			break;
+		case 0x2:
+			break;
+		case 0x3:
+			break;
+		case 0x4:
+			break;
+		case 0x5:
+			break;
+		case 0x6:
+			break;
+		case 0x7:
+			break;
+		case 0x8:
+			break;
+		case 0x9:
+			break;
+		case 0xA:
+			break;
+		case 0xB:
+			break;
+		case 0xC:
+			break;
+		case 0xD:
+			break;
+		case 0xE:
+			break;
+		case 0xF:
+			break;
+		default:
+			System.out.printf( "Opcode 0x%2x not valid or implemented opcode", opcode );
+			break;
+		}
+	}
+	
+	private void switch3LowNibble( byte opcode, byte lowNibble ) {
+		switch( lowNibble ) {
+		case 0x0:
+			break;
+		case 0x1:
+			break;
+		case 0x2:
+			break;
+		case 0x3:
+			break;
+		case 0x4:
+			break;
+		case 0x5:
+			break;
+		case 0x6:
+			break;
+		case 0x7:
+			break;
+		case 0x8:
+			break;
+		case 0x9:
+			break;
+		case 0xA:
+			break;
+		case 0xB:
+			break;
+		case 0xC:
+			break;
+		case 0xD:
+			break;
+		case 0xE:
+			break;
+		case 0xF:
+			break;
+		default:
+			System.out.printf( "Opcode 0x%2x not valid or implemented opcode", opcode );
+			break;
+		}
+	}
+	
+	private void switch4LowNibble( byte opcode, byte lowNibble ) {
+		switch( lowNibble ) {
+		case 0x0:
+			break;
+		case 0x1:
+			break;
+		case 0x2:
+			break;
+		case 0x3:
+			break;
+		case 0x4:
+			break;
+		case 0x5:
+			break;
+		case 0x6:
+			break;
+		case 0x7:
+			break;
+		case 0x8:
+			break;
+		case 0x9:
+			break;
+		case 0xA:
+			break;
+		case 0xB:
+			break;
+		case 0xC:
+			break;
+		case 0xD:
+			break;
+		case 0xE:
+			break;
+		case 0xF:
+			break;
+		default:
+			System.out.printf( "Opcode 0x%2x not valid or implemented opcode", opcode );
+			break;
+		}
+	}
+	
+	private void switch5LowNibble( byte opcode, byte lowNibble ) {
+		switch( lowNibble ) {
+		case 0x0:
+			break;
+		case 0x1:
+			break;
+		case 0x2:
+			break;
+		case 0x3:
+			break;
+		case 0x4:
+			break;
+		case 0x5:
+			break;
+		case 0x6:
+			break;
+		case 0x7:
+			break;
+		case 0x8:
+			break;
+		case 0x9:
+			break;
+		case 0xA:
+			break;
+		case 0xB:
+			break;
+		case 0xC:
+			break;
+		case 0xD:
+			break;
+		case 0xE:
+			break;
+		case 0xF:
+			break;
+		default:
+			System.out.printf( "Opcode 0x%2x not valid or implemented opcode", opcode );
+			break;
+		}
+	}
+	
+	private void switch6LowNibble( byte opcode, byte lowNibble ) {
+		switch( lowNibble ) {
+		case 0x0:
+			break;
+		case 0x1:
+			break;
+		case 0x2:
+			break;
+		case 0x3:
+			break;
+		case 0x4:
+			break;
+		case 0x5:
+			break;
+		case 0x6:
+			break;
+		case 0x7:
+			break;
+		case 0x8:
+			break;
+		case 0x9:
+			break;
+		case 0xA:
+			break;
+		case 0xB:
+			break;
+		case 0xC:
+			break;
+		case 0xD:
+			break;
+		case 0xE:
+			break;
+		case 0xF:
+			break;
+		default:
+			System.out.printf( "Opcode 0x%2x not valid or implemented opcode", opcode );
+			break;
+		}
+	}
+	
+	private void switch7LowNibble( byte opcode, byte lowNibble ) {
+		switch( lowNibble ) {
+		case 0x0:
+			break;
+		case 0x1:
+			break;
+		case 0x2:
+			break;
+		case 0x3:
+			break;
+		case 0x4:
+			break;
+		case 0x5:
+			break;
+		case 0x6:
+			break;
+		case 0x7:
+			break;
+		case 0x8:
+			break;
+		case 0x9:
+			break;
+		case 0xA:
+			break;
+		case 0xB:
+			break;
+		case 0xC:
+			break;
+		case 0xD:
+			break;
+		case 0xE:
+			break;
+		case 0xF:
+			break;
+		default:
+			System.out.printf( "Opcode 0x%2x not valid or implemented opcode", opcode );
+			break;
+		}
+	}
+	
+	private void switch8LowNibble( byte opcode, byte lowNibble ) {
+		switch( lowNibble ) {
+		case 0x0:
+			break;
+		case 0x1:
+			break;
+		case 0x2:
+			break;
+		case 0x3:
+			break;
+		case 0x4:
+			break;
+		case 0x5:
+			break;
+		case 0x6:
+			break;
+		case 0x7:
+			break;
+		case 0x8:
+			break;
+		case 0x9:
+			break;
+		case 0xA:
+			break;
+		case 0xB:
+			break;
+		case 0xC:
+			break;
+		case 0xD:
+			break;
+		case 0xE:
+			break;
+		case 0xF:
+			break;
+		default:
+			System.out.printf( "Opcode 0x%2x not valid or implemented opcode", opcode );
+			break;
+		}
+	}
+	
+	private void switch9LowNibble( byte opcode, byte lowNibble ) {
+		switch( lowNibble ) {
+		case 0x0:
+			break;
+		case 0x1:
+			break;
+		case 0x2:
+			break;
+		case 0x3:
+			break;
+		case 0x4:
+			break;
+		case 0x5:
+			break;
+		case 0x6:
+			break;
+		case 0x7:
+			break;
+		case 0x8:
+			break;
+		case 0x9:
+			break;
+		case 0xA:
+			break;
+		case 0xB:
+			break;
+		case 0xC:
+			break;
+		case 0xD:
+			break;
+		case 0xE:
+			break;
+		case 0xF:
+			break;
+		default:
+			System.out.printf( "Opcode 0x%2x not valid or implemented opcode", opcode );
+			break;
+		}
+	}
+	
+	private void switchALowNibble( byte opcode, byte lowNibble ) {
+		switch( lowNibble ) {
+		case 0x0:
+			break;
+		case 0x1:
+			break;
+		case 0x2:
+			break;
+		case 0x3:
+			break;
+		case 0x4:
+			break;
+		case 0x5:
+			break;
+		case 0x6:
+			break;
+		case 0x7:
+			break;
+		case 0x8:
+			break;
+		case 0x9:
+			break;
+		case 0xA:
+			break;
+		case 0xB:
+			break;
+		case 0xC:
+			break;
+		case 0xD:
+			break;
+		case 0xE:
+			break;
+		case 0xF:
+			break;
+		default:
+			System.out.printf( "Opcode 0x%2x not valid or implemented opcode", opcode );
+			break;
+		}
+	}
+	
+	private void switchBLowNibble( byte opcode, byte lowNibble ) {
+		switch( lowNibble ) {
+		case 0x0:
+			break;
+		case 0x1:
+			break;
+		case 0x2:
+			break;
+		case 0x3:
+			break;
+		case 0x4:
+			break;
+		case 0x5:
+			break;
+		case 0x6:
+			break;
+		case 0x7:
+			break;
+		case 0x8:
+			break;
+		case 0x9:
+			break;
+		case 0xA:
+			break;
+		case 0xB:
+			break;
+		case 0xC:
+			break;
+		case 0xD:
+			break;
+		case 0xE:
+			break;
+		case 0xF:
+			break;
+		default:
+			System.out.printf( "Opcode 0x%2x not valid or implemented opcode", opcode );
+			break;
+		}
+	}
+	
+	private void switchCLowNibble( byte opcode, byte lowNibble ) {
+		switch( lowNibble ) {
+		case 0x0:
+			break;
+		case 0x1:
+			break;
+		case 0x2:
+			break;
+		case 0x3:
+			break;
+		case 0x4:
+			break;
+		case 0x5:
+			break;
+		case 0x6:
+			break;
+		case 0x7:
+			break;
+		case 0x8:
+			break;
+		case 0x9:
+			break;
+		case 0xA:
+			break;
+		case 0xB:
+			break;
+		case 0xC:
+			break;
+		case 0xD:
+			break;
+		case 0xE:
+			break;
+		case 0xF:
+			break;
+		default:
+			System.out.printf( "Opcode 0x%2x not valid or implemented opcode", opcode );
+			break;
+		}
+	}
+	
+	private void switchDLowNibble( byte opcode, byte lowNibble ) {
+		switch( lowNibble ) {
+		case 0x0:
+			break;
+		case 0x1:
+			break;
+		case 0x2:
+			break;
+		case 0x3:
+			break;
+		case 0x4:
+			break;
+		case 0x5:
+			break;
+		case 0x6:
+			break;
+		case 0x7:
+			break;
+		case 0x8:
+			break;
+		case 0x9:
+			break;
+		case 0xA:
+			break;
+		case 0xB:
+			break;
+		case 0xC:
+			break;
+		case 0xD:
+			break;
+		case 0xE:
+			break;
+		case 0xF:
+			break;
+		default:
+			System.out.printf( "Opcode 0x%2x not valid or implemented opcode", opcode );
+			break;
+		}
+	}
+	
+	private void switchELowNibble( byte opcode, byte lowNibble ) {
+		switch( lowNibble ) {
+		case 0x0:
+			break;
+		case 0x1:
+			break;
+		case 0x2:
+			break;
+		case 0x3:
+			break;
+		case 0x4:
+			break;
+		case 0x5:
+			break;
+		case 0x6:
+			break;
+		case 0x7:
+			break;
+		case 0x8:
+			break;
+		case 0x9:
+			break;
+		case 0xA:
+			break;
+		case 0xB:
+			break;
+		case 0xC:
+			break;
+		case 0xD:
+			break;
+		case 0xE:
+			break;
+		case 0xF:
+			break;
+		default:
+			System.out.printf( "Opcode 0x%2x not valid or implemented opcode", opcode );
+			break;
+		}
+	}
+	
+	private void switchFLowNibble( byte opcode, byte lowNibble ) {
+		switch( lowNibble ) {
+		case 0x0:
+			break;
+		case 0x1:
+			break;
+		case 0x2:
+			break;
+		case 0x3:
+			break;
+		case 0x4:
+			break;
+		case 0x5:
+			break;
+		case 0x6:
+			break;
+		case 0x7:
+			break;
+		case 0x8:
+			break;
+		case 0x9:
+			break;
+		case 0xA:
+			break;
+		case 0xB:
+			break;
+		case 0xC:
+			break;
+		case 0xD:
+			break;
+		case 0xE:
+			break;
+		case 0xF:
+			break;
+		default:
+			System.out.printf( "Opcode 0x%2x not valid or implemented opcode", opcode );
+			break;
 		}
 	}
 }
