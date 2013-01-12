48bb0a977489b5a94648eca1950c60b98f3f05a5
John Ruffer <jqruffer@vt.edu>
2013-01-11 20:40:20 -0500

    First opcode class to be implemented

---
+++ src/gameBoy/cpu/opcodes/IncBc.java
@@ -0,0 +1,25 @@
+package gameBoy.cpu.opcodes;
+
+import gameBoy.cpu.GameBoyRegisters.Register;
+import gameBoy.interfaces.IOpcode;
+import gameBoy.interfaces.IProcessor;
+
+/**
+ * Command implementation of the opcode INC BC.
+ * @author john
+ *
+ */
+public class IncBc implements IOpcode {
+	private IProcessor processor;
+	
+	public IncBc( IProcessor processor ) {
+		this.processor = processor;
+	}
+	@Override
+	public void execute() {
+		// get register BC, increment it, and move it back into the register
+		this.processor.getRegisters().setRegister( Register.BC,
+				(short) ( this.processor.getRegisters().getRegister( Register.BC ) + 1 ) );
+	}
+
+}
