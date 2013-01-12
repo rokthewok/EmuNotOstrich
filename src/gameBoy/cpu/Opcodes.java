2f0fd55adc3b9a82d18eb26f8122ce1b53ee04f0
rokthewok <jqruffer@gmail.com>
2013-01-12 00:46:57 -0500

    Added more opcode constants

--- src/gameBoy/cpu/Opcodes.java
+++ src/gameBoy/cpu/Opcodes.java
@@ -41,12 +41,54 @@ public class Opcodes {
 	 */
 	public static int LD_BC_NN = 0x01;
 	public static int LD_ADDR_BC_A = 0x02;	// put A at address contained in BC
-	public static int LD_B_N = 0x06;
-	public static int LD_A_ADDR_BC = 0x0A;	
+	public static int LD_B_N = 0x06;	
 	public static int LD_ADDR_NN_SP = 0x08;
+	public static int LD_A_ADDR_BC = 0x0A;
+	public static int LD_C_N = 0x0E;
+	public static int LD_DE_NN = 0x11;
+	public static int LD_ADDR_DE_A = 0x12;
+	public static int LD_D_N = 0x16;
+	public static int LD_A_ADDR_DE = 0x1A;
+	public static int LD_E_N = 0x1E;
+	public static int LD_HL_NN = 0x21;
+	public static int LD_H_N = 0x26;
+	public static int LD_L_N = 0x2E;
+	public static int LD_SP_NN = 0x31;
+	public static int LD_ADDR_HL_N = 0x36;
+	public static int LD_A_N = 0x3E;
+	public static int LD_B_B = 0x40;
+	public static int LD_B_C = 0x41;
+	public static int LD_B_D = 0x42;
+	public static int LD_B_E = 0x43;
+	public static int LD_B_H = 0x44;
+	public static int LD_B_L = 0x45;
+	public static int LD_B_ADDR_HL = 0x46;
+	public static int LD_B_A = 0x47;
+	public static int LD_C_B = 0x48;
+	public static int LD_C_C = 0x49;
+	public static int LD_C_D = 0x4A;
+	public static int LD_C_E = 0x4B;
+	public static int LD_C_H = 0x4C;
+	public static int LD_C_L = 0x4D;
+	public static int LD_C_ADDR_HL = 0x4E;
+	public static int LD_C_A = 0x4F;
+	
 	
 	/*
 	 * Add opcodes (ADD)
 	 */
 	public static int ADD_HL_BC = 0x09;
+	public static int ADD_HL_DE = 0x19;
+	public static int ADD_HL_HL = 0x29;
+	public static int ADD_HL_SP = 0x39;
+	public static int ADD_A_B = 0x80;
+	public static int ADD_A_C = 0x81;
+	public static int ADD_A_D = 0x82;
+	public static int ADD_A_E = 0x83;
+	public static int ADD_A_H = 0x84;
+	public static int ADD_A_L = 0x85;
+	public static int ADD_A_ADDR_HL = 0x86;
+	public static int ADD_A_A = 0x87;
+	public static int ADD_A_N = 0xC6;
+	public static int ADD_SP_d = 0xE8;
 }
