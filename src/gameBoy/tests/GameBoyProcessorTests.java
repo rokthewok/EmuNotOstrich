f300d45638cb270d74e261e902e1ae5577d91352
John Ruffer <jqruffer@vt.edu>
2013-01-11 21:15:55 -0500

    Made made tests work after breaking them when reparing the GameBoyRegister class

--- src/gameBoy/tests/GameBoyProcessorTests.java
+++ src/gameBoy/tests/GameBoyProcessorTests.java
@@ -1,11 +1,10 @@
 package gameBoy.tests;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 public class GameBoyProcessorTests {
-
+	
 	@Test
 	public void test() {
 		fail("Not yet implemented");
