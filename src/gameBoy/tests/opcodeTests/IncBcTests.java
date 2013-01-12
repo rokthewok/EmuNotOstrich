94d73813ec7a16dbd31d2e900f02521b4034535a
John Ruffer <jqruffer@vt.edu>
2013-01-11 21:16:15 -0500

    filled out IncBc opcode test

--- src/gameBoy/tests/opcodeTests/IncBcTests.java
+++ src/gameBoy/tests/opcodeTests/IncBcTests.java
@@ -1,14 +1,35 @@
 package gameBoy.tests.opcodeTests;
 
 import static org.junit.Assert.*;
+import gameBoy.cpu.GameBoyProcessor;
+import gameBoy.cpu.GameBoyRegisters.Register;
+import gameBoy.cpu.opcodes.IncBc;
+import gameBoy.interfaces.IOpcode;
+import gameBoy.interfaces.IProcessor;
 
 import org.junit.Test;
 
 public class IncBcTests {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testExecution() {
+		short bcValue = 14;
+		IProcessor processor = new GameBoyProcessor();
+		processor.getRegisters().setRegister( Register.BC, bcValue );
+		IOpcode incBc = new IncBc( processor );
+		
+		incBc.execute();
+		bcValue++;
+		
+		assertEquals( bcValue, processor.getRegisters().getRegister( Register.BC ) );
+		
+		bcValue = 32514;
+		processor.getRegisters().setRegister( Register.BC, bcValue );
+		
+		incBc.execute();
+		bcValue++;
+		
+		assertEquals( bcValue, processor.getRegisters().getRegister( Register.BC ) );
 	}
 
 }
