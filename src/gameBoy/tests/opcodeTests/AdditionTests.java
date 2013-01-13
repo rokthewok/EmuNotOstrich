109c06500be94350fac842bf3688bf33db878ed6
rokthewok <jqruffer@gmail.com>
2013-01-13 13:11:55 -0500

    Completed all but one Addition test case

--- src/gameBoy/tests/opcodeTests/AdditionTests.java
+++ src/gameBoy/tests/opcodeTests/AdditionTests.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.*;
 import gameBoy.cpu.Flag;
 import gameBoy.cpu.GameBoyProcessor;
 import gameBoy.cpu.Register;
-import gameBoy.cpu.opcodes.add.AddAB;
+import gameBoy.cpu.opcodes.add.*;
 import gameBoy.interfaces.IOpcode;
 import gameBoy.interfaces.IProcessor;
 
@@ -13,9 +13,63 @@ import org.junit.Test;
 public class AdditionTests {
 
 	@Test
-	public void testAddABExecution() {
+	public void testAddAB() {
 		IProcessor processor = new GameBoyProcessor();
 		IOpcode add = new AddAB( processor );
+		
+		this.do8BitTest( Register.B, add, processor );
+
+	}
+
+	@Test
+	public void testAddAC() {
+		IProcessor processor = new GameBoyProcessor();
+		IOpcode add = new AddAC( processor );
+		
+		this.do8BitTest( Register.C, add, processor );
+
+	}
+	
+	@Test
+	public void testAddAD() {
+		IProcessor processor = new GameBoyProcessor();
+		IOpcode add = new AddAD( processor );
+		
+		this.do8BitTest( Register.D, add, processor );
+
+	}
+	
+	@Test
+	public void testAddAE() {
+		IProcessor processor = new GameBoyProcessor();
+		IOpcode add = new AddAE( processor );
+		
+		this.do8BitTest( Register.E, add, processor );
+
+	}
+	
+	@Test
+	public void testAddAH() {
+		IProcessor processor = new GameBoyProcessor();
+		IOpcode add = new AddAH( processor );
+		
+		this.do8BitTest( Register.H, add, processor );
+
+	}
+	
+	@Test
+	public void testAddAL() {
+		IProcessor processor = new GameBoyProcessor();
+		IOpcode add = new AddAL( processor );
+		
+		this.do8BitTest( Register.L, add, processor );
+
+	}
+	
+	@Test
+	public void testAddAA() {
+		IProcessor processor = new GameBoyProcessor();
+		IOpcode add = new AddAA( processor );
 		
 		add.execute();
 		
@@ -26,7 +80,6 @@ public class AdditionTests {
 		assertEquals( 0, processor.getRegisters().getFlag( Flag.N ) );
 		
 		processor.getRegisters().setRegister( Register.A, (byte) 1 );
-		processor.getRegisters().setRegister( Register.B, (byte) 1 );
 		
 		add.execute();
 		
@@ -36,7 +89,7 @@ public class AdditionTests {
 		assertEquals( 0, processor.getRegisters().getFlag( Flag.H ) );
 		assertEquals( 0, processor.getRegisters().getFlag( Flag.N ) );
 		
-		processor.getRegisters().setRegister( Register.B, (byte) 14 );
+		processor.getRegisters().setRegister( Register.A, (byte) 8 );
 		
 		add.execute();
 		
@@ -47,7 +100,49 @@ public class AdditionTests {
 		assertEquals( 0, processor.getRegisters().getFlag( Flag.N ) );
 		
 		processor.getRegisters().setRegister( Register.A, (byte) 127 );
-		processor.getRegisters().setRegister( Register.B, (byte) 1 );
+		
+		add.execute();
+		
+		assertEquals( -2, processor.getRegisters().getRegister( Register.A ) );
+		assertEquals( 1, processor.getRegisters().getFlag( Flag.C ) );
+		assertEquals( 0, processor.getRegisters().getFlag( Flag.Z ) );
+		assertEquals( 1, processor.getRegisters().getFlag( Flag.H ) );
+		assertEquals( 0, processor.getRegisters().getFlag( Flag.N ) );
+
+	}
+	
+	private void do8BitTest( Register register, IOpcode add, IProcessor processor ) {
+		add.execute();
+		
+		assertEquals( 0, processor.getRegisters().getRegister( Register.A ) );
+		assertEquals( 0, processor.getRegisters().getFlag( Flag.C ) );
+		assertEquals( 1, processor.getRegisters().getFlag( Flag.Z ) );
+		assertEquals( 0, processor.getRegisters().getFlag( Flag.H ) );
+		assertEquals( 0, processor.getRegisters().getFlag( Flag.N ) );
+		
+		processor.getRegisters().setRegister( Register.A, (byte) 1 );
+		processor.getRegisters().setRegister( register, (byte) 1 );
+		
+		add.execute();
+		
+		assertEquals( 2, processor.getRegisters().getRegister( Register.A ) );
+		assertEquals( 0, processor.getRegisters().getFlag( Flag.C ) );
+		assertEquals( 0, processor.getRegisters().getFlag( Flag.Z ) );
+		assertEquals( 0, processor.getRegisters().getFlag( Flag.H ) );
+		assertEquals( 0, processor.getRegisters().getFlag( Flag.N ) );
+		
+		processor.getRegisters().setRegister( register, (byte) 14 );
+		
+		add.execute();
+		
+		assertEquals( 16, processor.getRegisters().getRegister( Register.A ) );
+		assertEquals( 0, processor.getRegisters().getFlag( Flag.C ) );
+		assertEquals( 0, processor.getRegisters().getFlag( Flag.Z ) );
+		assertEquals( 1, processor.getRegisters().getFlag( Flag.H ) );
+		assertEquals( 0, processor.getRegisters().getFlag( Flag.N ) );
+		
+		processor.getRegisters().setRegister( Register.A, (byte) 127 );
+		processor.getRegisters().setRegister( register, (byte) 1 );
 		
 		add.execute();
 		
@@ -57,5 +152,4 @@ public class AdditionTests {
 		assertEquals( 1, processor.getRegisters().getFlag( Flag.H ) );
 		assertEquals( 0, processor.getRegisters().getFlag( Flag.N ) );
 	}
-
 }
