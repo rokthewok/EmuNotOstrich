c2ce498c68606f27fe579956bf0011f546de1311
rokthewok <jqruffer@gmail.com>
2013-01-14 10:31:22 -0500

    Added suites to the alltests class

--- src/gameBoy/tests/AllTests.java
+++ src/gameBoy/tests/AllTests.java
@@ -1,13 +1,13 @@
 package gameBoy.tests;
 
-import gameBoy.tests.opcodeTests.IncrementTests;
-
+import gameBoy.tests.opcodeTests.AllOpcodeTests;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ GameBoyProcessorTests.class, GameBoyRegistersTests.class, MemoryTests.class, IncrementTests.class })
+@SuiteClasses({ GameBoyProcessorTests.class, GameBoyRegistersTests.class,
+				MemoryTests.class, AllOpcodeTests.class })
 public class AllTests {
 
 }
