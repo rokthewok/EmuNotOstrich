9e1db5ce4f65bc6eed16e80107b2e177eb448b88
rokthewok <jqruffer@gmail.com>
2013-01-14 11:31:41 -0500

    Added tests for getters and setters in GameBoyProcessorTests. Execution needs to be tested

--- src/gameBoy/tests/GameBoyProcessorTests.java
+++ src/gameBoy/tests/GameBoyProcessorTests.java
@@ -1,13 +1,25 @@
 package gameBoy.tests;
 
 import static org.junit.Assert.*;
+import gameBoy.cpu.GameBoyProcessor;
+import gameBoy.cpu.GameBoyRegisters;
+import gameBoy.interfaces.IMemory;
+import gameBoy.interfaces.IProcessor;
+import gameBoy.interfaces.IRegister;
+import gameBoy.memory.Memory;
+
 import org.junit.Test;
 
 public class GameBoyProcessorTests {
 	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGetAndSet() {
+		IMemory memory = new Memory();
+		IRegister registers = new GameBoyRegisters();
+		IProcessor procesor = new GameBoyProcessor( registers, memory );
+		
+		assertSame( memory, procesor.getMemory() );
+		assertSame( registers, procesor.getRegisters() );
 	}
 
 }
